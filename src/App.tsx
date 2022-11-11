339b1829289d8934b6b89299409f7805bf665b63
Eunhyung Choi <milkiss0414@naver.com>
2022-11-11 21:36:49 +0900

    Update: greeting 메시지

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+interface iMessage {
+  name?: string;
+  content: string;
+}
+
 function App() {
-  return <div className="App">❄️❄️❄️❄️❄️❄️❄️❄️</div>;
+  const message: iMessage = {
+    name: "은형",
+    content: "❄️겨울왕국에 오신 것을 환영합니다❄️",
+  };
+
+  return (
+    <div className="App">
+      <h1>{message.content}</h1>
+      <h2>-By {message.name}</h2>
+    </div>
+  );
 }
 
 export default App;
