e69b24d8208c42038b04cf9253459e28c4949e8c
Eunhyung Choi <milkiss0414@naver.com>
2022-11-14 00:15:18 +0900

    Update: GlobalStyle 폰트 추가

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,8 +2,13 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
+    @import "//cdn.jsdelivr.net/npm/font-applesdgothicneo@1.0/all.min.css";
+
     ${reset}
     // 아래에 전역 스타일 추가
+    * {
+        font-family: 'Apple Sandoll Gothic Neo';
+    }
 `;
 
 export default GlobalStyle;
