0f196bf0994b48b90912b06f596aa20beb3fb55a
Eunhyung Choi <milkiss0414@naver.com>
2022-11-14 01:34:29 +0900

    Update: MobileContainer 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Router from "./Router";
 import GlobalStyle from "./styles/GlobalStyle";
+import styled from "styled-components";
+
+const MobileContainer = styled.div`
+  position: relative;
+
+  margin: 0 auto;
+
+  width: 375px;
+`;
 
 function App() {
   return (
-    <>
+    <MobileContainer>
       <GlobalStyle />
       <Router />
-    </>
+    </MobileContainer>
   );
 }
 
