fe89ed7026cd38562cc68f692258f91c3e3aaa36
wooyoung0830 <thddndud0830@gmail.com>
2022-11-14 11:23:06 +0900

    FEAT: main page button

---
+++ src/components/MainPage/Filter.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+const FilterButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 16px 10px 16px 20px;
+`;
+
+const RecommendationButton = styled.button`
+  width: 74px;
+  height: 40px;
+  background-color: #d9d9d9;
+  ${theme.fonts.body3};
+  color: ${theme.colors.white};
+  padding: 10px;
+  border-radius: 20px;
+`;
+
+const BurgerButton = styled.button`
+  width: 87px;
+  height: 40px;
+  background-color: #d9d9d9;
+  ${theme.fonts.body3};
+  color: ${theme.colors.white};
+  padding: 10px;
+  border-radius: 20px;
+`;
+const SnackButton = styled.button`
+  width: 99px;
+  height: 40px;
+  background-color: #d9d9d9;
+  ${theme.fonts.body3};
+  color: ${theme.colors.white};
+  padding: 10px;
+  border-radius: 20px;
+`;
+const DrinkButton = styled.button`
+  width: 46px;
+  height: 40px;
+  background-color: #d9d9d9;
+  ${theme.fonts.body3};
+  color: ${theme.colors.white};
+  padding: 10px;
+  border-radius: 20px;
+`;
+
+export default function Filter() {
+  const data = [
+    {
+      menuId: 1,
+      menuName: "트리플 치즈 버거",
+      priceOnly: 5700,
+    },
+    {
+      menuId: 2,
+      menuName: "페퍼로니 치즈 버거",
+      priceOnly: 5700,
+    },
+    {
+      menuId: 3,
+      menuName: "페퍼로니 치즈 버거",
+      priceOnly: 5700,
+    },
+    {
+      menuId: 4,
+      menuName: "페퍼로니 치즈 버거",
+      priceOnly: 5700,
+    },
+  ];
+  return (
+    <FilterButtonWrapper>
+      <RecommendationButton>추천 메뉴</RecommendationButton>
+      <BurgerButton>버거&세트</BurgerButton>
+      <SnackButton>스낵&사이드</SnackButton>
+      <DrinkButton>음료</DrinkButton>
+    </FilterButtonWrapper>
+  );
+}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,32 @@
 // 메뉴 전체 보여줄 메인페이지
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import theme from "../styles/theme";
+import Filter from "../components/MainPage/Filter";
 
+const MainBackground = styled.div`
+  width: 375px;
+  height: auto;
+  background-color: ${theme.colors.bg};
+`;
+
+const BuyButton = styled.button`
+  width: 335px;
+  height: 50px;
+  background-color: #dddddd;
+  ${theme.fonts.title2};
+  color: ${theme.colors.white};
+  border-radius: 10px;
+  border: none;
+`;
 export default function MainPage() {
-  return <div>MainPage</div>;
+  const [quantity, setQuantity] = useState<number>(0);
+  return (
+    <>
+      <MainBackground>
+        <Filter />
+        <BuyButton>구매하기 ({quantity})</BuyButton>
+      </MainBackground>
+    </>
+  );
 }
