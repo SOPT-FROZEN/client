2ac9b59005d9531cf9e2535f2d60dac08ed2e328
Eunhyung Choi <milkiss0414@naver.com>
2022-11-19 00:25:58 +0900

    Fix: 삭제버튼 targetIndex 도출

---
+++ src/pages/CartPage.tsx
@@ -0,0 +1,154 @@
+// 장바구니 페이지
+import React, { useState } from "react";
+import styled from "styled-components";
+import AddressAndTime from "../components/CartPage/AddressAndTime";
+import Cards from "../components/CartPage/Cards";
+import theme from "../styles/theme";
+import { CartEmpty } from "../assets/image/asset";
+
+const CartWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  padding: 17.2px 20px;
+  padding-bottom: 0px;
+
+  min-height: 100vh; // 수정 요망
+
+  background-color: ${theme.colors.bg};
+`;
+
+const Title = styled.header`
+  margin-bottom: 25px;
+
+  ${theme.fonts.title1};
+`;
+
+const EmptyContainer = styled.article`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  margin-top: 132px;
+  margin-bottom: 226px;
+
+  > img {
+    width: 145px;
+    height: 108px;
+  }
+`;
+
+const AddBtnContainer = styled.button`
+  width: 100%;
+  margin-top: 10px;
+  > button {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 42px;
+
+    background-color: ${theme.colors.gray300};
+    border: 1px solid #a5a5a5;
+    border-radius: 10px;
+
+    ${theme.fonts.caption1};
+    color: ${theme.colors.gray700};
+    cursor: pointer;
+  }
+`;
+
+const OkBtn = styled.button`
+  height: 50px;
+  width: 100%;
+
+  padding: 10px;
+  margin-top: 15px;
+  margin-bottom: 24px;
+
+  background: #d14938;
+  border-radius: 10px;
+  filter: drop-shadow(0px 3px 13px rgba(0, 0, 0, 0.25));
+
+  color: ${theme.colors.white};
+  ${theme.fonts.title2};
+
+  cursor: pointer;
+`;
+
+export interface iSet {
+  setId: number;
+  set: string;
+  price: number;
+  amount: number;
+}
+export interface iOrder {
+  menuId: number;
+  title: string;
+  total: number;
+  details?: iSet[];
+}
+
+export default function CartPage() {
+  const [orders, setOrders] = useState<iOrder[]>([
+    {
+      menuId: 1,
+      title: "트리플 치즈 버거",
+      total: 18900,
+      details: [
+        {
+          setId: 1,
+          set: "라지 세트",
+          price: 9900,
+          amount: 1,
+        },
+        {
+          setId: 2,
+          set: "단품",
+          price: 4500,
+          amount: 2,
+        },
+      ],
+    },
+    {
+      menuId: 2,
+      title: "더블 치즈 버거",
+      total: 9900,
+      details: [
+        {
+          setId: 1,
+          set: "라지 세트",
+          price: 9900,
+          amount: 1,
+        },
+        {
+          setId: 2,
+          set: "단품",
+          price: 4500,
+          amount: 2,
+        },
+      ],
+    },
+  ]);
+  return (
+    <CartWrapper>
+      <Title>장바구니</Title>
+      {orders.length ? (
+        <div>
+          <Cards orders={orders} setOrders={setOrders} />
+          <AddBtnContainer>
+            <button>항목 추가</button>
+          </AddBtnContainer>
+          <AddressAndTime />
+          <OkBtn>주문확인</OkBtn>
+        </div>
+      ) : (
+        <div>
+          <EmptyContainer>
+            <img src={CartEmpty} alt="빈 장바구니 이미지" />
+          </EmptyContainer>
+          <OkBtn>항목 추가하기</OkBtn>
+        </div>
+      )}
+    </CartWrapper>
+  );
+}
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,115 @@
+// 메뉴 전체 보여줄 메인페이지
+import React, { useState } from "react";
+import styled from "styled-components";
+import theme from "../styles/theme";
+import Filter from "../components/MainPage/Filter";
+import Memu from "../components/MainPage/Memu";
+import { burger_image } from "../assets/image/asset";
+
+export interface ItemProps {
+  menuId: number;
+  menuName: string;
+  image: string;
+  priceOnly: number;
+}
+
+export default function MainPage() {
+  const [quantity, setQuantity] = useState<number>(0);
+  const data = [
+    {
+      menuId: 1,
+      menuName: "트리플 치즈 버거",
+      image: burger_image,
+      priceOnly: 5700,
+    },
+    {
+      menuId: 2,
+      menuName: "맥스파이시 버거",
+      image: burger_image,
+      priceOnly: 7500,
+    },
+    {
+      menuId: 3,
+      menuName: "페퍼로니 치즈 버거",
+      image: burger_image,
+      priceOnly: 5700,
+    },
+    {
+      menuId: 4,
+      menuName: "1955 버거",
+      image: burger_image,
+      priceOnly: 8500,
+    },
+    {
+      menuId: 5,
+      menuName: "트리플 치즈 버거",
+      image: burger_image,
+      priceOnly: 5700,
+    },
+    {
+      menuId: 6,
+      menuName: "페퍼로니 치즈 버거",
+      image: burger_image,
+      priceOnly: 5700,
+    },
+    {
+      menuId: 7,
+      menuName: "페퍼로니 치즈 버거",
+      image: burger_image,
+      priceOnly: 5700,
+    },
+    {
+      menuId: 7,
+      menuName: "페퍼로니 치즈 버거",
+      image: burger_image,
+      priceOnly: 5700,
+    },
+  ];
+  const [menus, setMenus] = useState<ItemProps[]>(data);
+  return (
+    <>
+      <MainBackground>
+        <Filter />
+        <MemuWrapper>
+          {menus.map((item: ItemProps) => {
+            return <Memu item={item} key={item.menuId} />;
+          })}
+        </MemuWrapper>
+        <BuyButton>구매하기 ({quantity})</BuyButton>
+      </MainBackground>
+    </>
+  );
+}
+
+const MainBackground = styled.div`
+  width: 375px;
+  height: auto;
+  background-color: ${theme.colors.bg};
+  padding-top: 10px;
+`;
+
+const MemuWrapper = styled.div`
+  width: 335px;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 20px;
+`;
+
+const BuyButton = styled.button`
+  width: 335px;
+  height: 50px;
+  background-color: #dddddd;
+  ${theme.fonts.title2};
+  color: ${theme.colors.white};
+  border-radius: 10px;
+  border: none;
+  position: fixed;
+  bottom: 26.06px;
+  z-index: 1000;
+  margin: 0 20px;
+  box-shadow: 0 3px 13px 0 rgba(0, 0, 0, 0.25);
+`;
