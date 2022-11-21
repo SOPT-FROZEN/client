8a0759730550830948c456fb675342a562f825ea
Eunhyung Choi <milkiss0414@naver.com>
2022-11-22 02:42:20 +0900

    Refact: AddressTime 스타일컴포넌트

--- src/components/CartPage/AddressAndTime.tsx
+++ src/components/CartPage/AddressAndTime.tsx
@@ -2,6 +2,25 @@ import React from "react";
 import styled from "styled-components";
 import theme from "../../styles/theme";
 
+export default function AddressAndTime() {
+  return (
+    <AddressTimeWrapper>
+      <AddressTimeContainer>
+        <AddressTimeTitle>이 주소로 배달됩니다.</AddressTimeTitle>
+        <AddressTimeSubtitle>서울특별시 동대문구 회기동 123-45 101호</AddressTimeSubtitle>
+        <ChangeBtn>변경</ChangeBtn>
+      </AddressTimeContainer>
+      <AddressTimeContainer>
+        <AddressTimeTitle>예상 배달 시간</AddressTimeTitle>
+        <AddressTimeSubtitle>2022/01/11 07:54</AddressTimeSubtitle>
+        <ChangeBtn>변경</ChangeBtn>
+      </AddressTimeContainer>
+    </AddressTimeWrapper>
+  );
+}
+
+const AddressTimeWrapper = styled.section``;
+
 const AddressTimeContainer = styled.article`
   display: flex;
   flex-direction: column;
@@ -10,48 +29,32 @@ const AddressTimeContainer = styled.article`
 
   background-color: ${theme.colors.white};
   border-radius: 5px;
-  > h1 {
-    margin-left: 10px;
-    ${theme.fonts.title2};
-  }
-
-  > h2 {
-    margin-left: 15px;
-    margin-top: 3px;
-    ${theme.fonts.body2};
-    font-weight: 400;
-    color: #6d6d6d;
-  }
-
-  > button {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    padding: 10px;
-    margin: 0px 12px;
-    margin-top: 13px;
-
-    border: 1px solid #545454;
-    border-radius: 10px;
-
-    cursor: pointer;
-  }
 `;
 
-export default function AddressAndTime() {
-  return (
-    <div>
-      <AddressTimeContainer>
-        <h1>이 주소로 배달됩니다.</h1>
-        <h2>서울특별시 동대문구 회기동 123-45 101호</h2>
-        <button>변경</button>
-      </AddressTimeContainer>
-      <AddressTimeContainer>
-        <h1>예상 배달 시간</h1>
-        <h2>2022/01/11 07:54</h2>
-        <button>변경</button>
-      </AddressTimeContainer>
-    </div>
-  );
-}
+const AddressTimeTitle = styled.h1`
+  margin-left: 10px;
+  ${theme.fonts.title2};
+`;
+
+const AddressTimeSubtitle = styled.h2`
+  margin-left: 15px;
+  margin-top: 3px;
+  ${theme.fonts.body2};
+  font-weight: 400;
+  color: #6d6d6d;
+`;
+
+const ChangeBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 10px;
+  margin: 0px 12px;
+  margin-top: 13px;
+
+  border: 1px solid #545454;
+  border-radius: 10px;
+
+  cursor: pointer;
+`;
--- src/components/CartPage/CardContent/CardMenu.tsx
+++ src/components/CartPage/CardContent/CardMenu.tsx
@@ -3,61 +3,63 @@ import styled from "styled-components";
 import { CartMenuImage } from "../../../assets/image/asset";
 import theme from "../../../styles/theme";
 
-const CardContentMenu = styled.article`
-  display: flex;
-
-  > img {
-    margin-top: 1.3px;
-    width: 104px;
-    height: 95px;
-
-    border: 1px solid ${theme.colors.gray200};
-    border-radius: 5px;
-  }
-
-  > div {
-    width: 100%;
-
-    > h3 {
-      margin-top: 11px;
-      margin-left: 14px;
-
-      ${theme.fonts.caption1};
-      > span {
-        margin-left: 35px;
-        ${theme.fonts.caption1};
-        font-weight: 500;
-        color: ${theme.colors.green};
-      }
-    }
-
-    > ul {
-      margin-top: 11px;
-      margin-left: 30px;
-      list-style: disc outside;
-      ${theme.fonts.caption1};
-      font-weight: 500;
-      color: #0a0909;
-      > li:last-child {
-        margin-top: 3px;
-      }
-    }
-  }
-`;
 export default function CardMenu(detail: { set: string; price: number }) {
   return (
     <CardContentMenu>
-      <img src={CartMenuImage} alt="메뉴이미지" />
-      <div>
-        <h3>
+      <MenuImg src={CartMenuImage} alt="메뉴이미지" />
+      <DetailContainer>
+        <DetailTitle>
           {detail.set}
           <span>₩ {detail.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
-        </h3>
-        <ul>
+        </DetailTitle>
+        <DetailOptions>
           <li>사이드 - 후렌치 후라이</li>
           <li>음료 - 코카콜라</li>
-        </ul>
-      </div>
+        </DetailOptions>
+      </DetailContainer>
     </CardContentMenu>
   );
 }
+
+const CardContentMenu = styled.article`
+  display: flex;
+`;
+
+const MenuImg = styled.img`
+  margin-top: 1.3px;
+  width: 104px;
+  height: 95px;
+
+  border: 1px solid ${theme.colors.gray200};
+  border-radius: 5px;
+`;
+
+const DetailContainer = styled.div`
+  width: 100%;
+`;
+
+const DetailTitle = styled.h3`
+  margin-top: 11px;
+  margin-left: 14px;
+
+  ${theme.fonts.caption1};
+
+  > span {
+    margin-left: 35px;
+    ${theme.fonts.caption1};
+    font-weight: 500;
+    color: ${theme.colors.green};
+  }
+`;
+
+const DetailOptions = styled.ul`
+  margin-top: 11px;
+  margin-left: 30px;
+  list-style: disc outside;
+  ${theme.fonts.caption1};
+  font-weight: 500;
+  color: #0a0909;
+  > li:last-child {
+    margin-top: 3px;
+  }
+`;
